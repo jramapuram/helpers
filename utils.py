b0f9c13ae5ebf11630bd61bb1f93dcb006a1f622
Jason Ramapuram <jason.ramapuram@gmail.com>
2020-05-04 11:21:19 +0200

    slurm-job id should not be part of filename

--- utils.py
+++ utils.py
@@ -582,7 +582,7 @@ def get_name(args):
                       'input_shape', 'fid_model_dir', 'output_dir', 'gpu',
                       'num_train_samples', 'num_test_samples', 'num_valid_samples', 'workers_per_replica',
                       'steps_per_train_epoch', 'total_train_steps', 'distributed_master', 'distributed_port',
-                      'distributed_rank', 'multi_gpu_distributed']
+                      'distributed_rank', 'multi_gpu_distributed', 'slurm_job_id']
     filtered = {k: v for k, v in vargs.items() if k not in blacklist_keys}  # remove useless info
 
     def _factor(name):
