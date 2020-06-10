8cfbf607310074328a99442143307d078fd455ad
Jason Ramapuram <jason.ramapuram@gmail.com>
2020-06-10 23:18:30 +0200

    use DETR attention

--- layers.py
+++ layers.py
@@ -6,8 +6,10 @@ import functools
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+
+from copy import deepcopy
 from torchvision import models
-from typing import Tuple, Union
+from typing import Tuple, Union, Optional
 from collections import OrderedDict
 
 from .s3d import S3D
@@ -1324,60 +1326,101 @@ class DenseResnet(nn.Module):
         return self.act(out)
 
 
-class AttentionBlock(nn.Module):
-    def __init__(self, input_shape, embedding_size, num_heads=1, normalization_str="none", **kwargs):
-        """ An attention block that takes an input [B, input_size] and returns [B, embedding_size].
-            S: source seq len (assumed 1)
-            L: target seq len (assumed 1)
-            N: batch_size
-            E: embedding size
+class TransformerEncoder(nn.Module):
+    """Modified from DETR: https://bit.ly/2Morbid ."""
 
-        :param input_shape: the input shape of the tensor
-        :param embedding_size: the output size (equivalent to embedding size)
-        :param num_heads: the number of heads to use in parallel, NOTE: needs to be divisible by embedding_size
-        :param normalization_str: the type of dense normalization to use
-        :returns: AttentionBlock Module
-        :rtype: nn.Module
+    def __init__(self, num_layers, embedding_size, nhead=8, latent_size=2048, dropout=0.1,
+                 activation_str="relu", norm_first_layer: bool = False, norm_last_layer: bool = False):
+        super(TransformerEncoder, self).__init__()
+        encoder_layer = TransformerEncoderLayer(embedding_size=embedding_size,
+                                                nhead=nhead,
+                                                latent_size=latent_size,
+                                                dropout=dropout,
+                                                activation_str=activation_str,
+                                                norm_first_layer=norm_first_layer)
+        self.layers = self._get_clones(encoder_layer, num_layers)
+        self.num_layers = num_layers
+        self.norm = nn.LayerNorm(embedding_size) if norm_last_layer else None
 
-        """
-        super(AttentionBlock, self).__init__()
-        # (S, N, E)
-        self.key_net = nn.Sequential(
-            _build_dense(input_shape, embedding_size, num_layers=3,
-                         normalization_str=normalization_str, **kwargs),
-            View([1, -1, embedding_size])
-        )
+    def _get_clones(self, module, N):
+        return nn.ModuleList([deepcopy(module) for i in range(N)])
 
-        # (L, N, E)
-        self.query_net = nn.Sequential(
-            _build_dense(input_shape, embedding_size, num_layers=3,
-                         normalization_str=normalization_str, **kwargs),
-            View([1, -1, embedding_size])
-        )
+    def forward(self, src,
+                mask: Optional[torch.Tensor] = None,
+                src_key_padding_mask: Optional[torch.Tensor] = None,
+                pos: Optional[torch.Tensor] = None):
+        output = src
 
-        # (S, N, E)
-        self.value_net = nn.Sequential(
-            _build_dense(input_shape, embedding_size, num_layers=3,
-                         normalization_str=normalization_str, **kwargs),
-            View([1, -1, embedding_size])
-        )
+        for layer in self.layers:
+            output = layer(output, src_mask=mask,
+                           src_key_padding_mask=src_key_padding_mask, pos=pos)
 
-        self.attn = nn.MultiheadAttention(embedding_size, num_heads=num_heads)
+        if self.norm is not None:
+            output = self.norm(output)
 
-    def forward(self, x):
-        """ Takes an input x, projects to key-dim, query-dim and value-dim and runs attn.
+        return output
 
-        :param x: input tensor [B, input_size]
-        :returns: output tensor [B, embedding_size]
-        :rtype: torch.Tensor
 
-        """
-        key = self.key_net(x)
-        query = self.query_net(x)
-        value = self.value_net(x)
+class TransformerEncoderLayer(nn.Module):
+    """Modified from DETR: https://bit.ly/2Morbid ."""
 
-        out = self.attn(query, key, value, need_weights=False)[0]
-        return out.squeeze()
+    def __init__(self, embedding_size, nhead, latent_size=2048, dropout=0.1,
+                 activation_str="relu", norm_first_layer: bool = False):
+        super(TransformerEncoderLayer, self).__init__()
+        self.self_attn = nn.MultiheadAttention(embedding_size, nhead, dropout=dropout)
+        # Implementation of Feedforward model
+        self.linear1 = nn.Linear(embedding_size, latent_size)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(latent_size, embedding_size)
+
+        self.norm1 = nn.LayerNorm(embedding_size)
+        self.norm2 = nn.LayerNorm(embedding_size)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+        self.activation = str_to_activ_module(activation_str)()
+        self.normalize_before = norm_first_layer
+
+    def with_pos_embed(self, tensor, pos: Optional[torch.Tensor]):
+        return tensor if pos is None else tensor + pos
+
+    def forward_post(self,
+                     src,
+                     src_mask: Optional[torch.Tensor] = None,
+                     src_key_padding_mask: Optional[torch.Tensor] = None,
+                     pos: Optional[torch.Tensor] = None):
+        q = k = self.with_pos_embed(src, pos)
+        src2 = self.self_attn(q, k, value=src, attn_mask=src_mask,
+                              key_padding_mask=src_key_padding_mask)[0]
+        src = src + self.dropout1(src2)
+        src = self.norm1(src)
+        src2 = self.linear2(self.dropout(self.activation(self.linear1(src))))
+        src = src + self.dropout2(src2)
+        src = self.norm2(src)
+        return src
+
+    def forward_pre(self, src,
+                    src_mask: Optional[torch.Tensor] = None,
+                    src_key_padding_mask: Optional[torch.Tensor] = None,
+                    pos: Optional[torch.Tensor] = None):
+        src2 = self.norm1(src)
+        q = k = self.with_pos_embed(src2, pos)
+        src2 = self.self_attn(q, k, value=src2, attn_mask=src_mask,
+                              key_padding_mask=src_key_padding_mask)[0]
+        src = src + self.dropout1(src2)
+        src2 = self.norm2(src)
+        src2 = self.linear2(self.dropout(self.activation(self.linear1(src2))))
+        src = src + self.dropout2(src2)
+        return src
+
+    def forward(self, src,
+                src_mask: Optional[torch.Tensor] = None,
+                src_key_padding_mask: Optional[torch.Tensor] = None,
+                pos: Optional[torch.Tensor] = None):
+        if self.normalize_before:
+            return self.forward_pre(src, src_mask, src_key_padding_mask, pos)
+
+        return self.forward_post(src, src_mask, src_key_padding_mask, pos)
 
 
 class BasicBlock(nn.Module):
